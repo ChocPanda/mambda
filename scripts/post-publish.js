c10846166f26851fdba09edb67c4bc7501d21de7
Matt Searle <mattsearle@ymail.com>
2019-07-07 15:26:43 +0100

    fix(publish): Attempt to fix post publish script
    re #29

--- scripts/post-publish.js
+++ scripts/post-publish.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 
 // Destination will be created or overwritten by default.
-fs.copyFile('./dist/package.json', './package.json', err => {
+fs.copyFile('./package.json', '../package.json', err => {
 	if (err) throw err;
 	console.log('Copied updated package.json');
 });
 
-fs.copyFile('./dist/CHANGELOG.md', 'CHANGELOG.md', err => {
+fs.copyFile('./CHANGELOG.md', '../CHANGELOG.md', err => {
 	if (err) throw err;
 	console.log('Copied updated CHANGELOG');
 });
