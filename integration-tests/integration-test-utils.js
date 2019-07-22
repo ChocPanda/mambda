9892b0e227fe084c991e31233cf6c61bd5d4a8e4
Matt Searle <mattsearle@ymail.com>
2019-07-22 13:04:20 +0100

    test(integration): Implement an example integration test
    re #42

---
+++ integration-tests/integration-test-utils.js
@@ -0,0 +1,102 @@
+const AWS = require('aws-sdk');
+const JSZip = require("jszip");
+const fs = require('fs');
+const util = require('util');
+const logger = console;
+const iam = new AWS.IAM({ region: process.env.AWS_DEFAULT_REGION });
+const readFile = util.promisify(fs.readFile);
+const lambda = new AWS.Lambda({ region: process.env.AWS_DEFAULT_REGION });
+
+const lambda_trust = {
+    Version: "2012-10-17",
+    Statement: [
+        {
+            Effect: "Allow",
+            Principal: {
+                Service: "lambda.amazonaws.com"
+            },
+            Action: "sts:AssumeRole"
+        }
+    ]
+}
+
+const withRole = (roleName, policyDoc) => async continuation => {
+    const fullRoleName = `${roleName}-Role`;
+    logger.info(`Creating role: ${fullRoleName}`);
+    const {
+        Role: {
+            Arn: arn
+        }
+    } = await iam.createRole({
+        AssumeRolePolicyDocument: JSON.stringify(lambda_trust),
+        Path: "/",
+        RoleName: fullRoleName
+    }).promise();
+    try {
+        logger.info(`Created role with arn: ${arn}`);
+        await withPolicy(roleName, policyDoc, arn)(continuation);
+    } finally {
+        logger.info(`Deleting role`);
+        await iam.deleteRole({ RoleName: fullRoleName }).promise();
+        logger.info(`Role deleted`);
+    }
+};
+
+const withPolicy = (roleName, policyDoc, arn) => async continuation => {
+    const fullRoleName = `${roleName}-Role`;
+    const policyName = `${roleName}-Policy`;
+    logger.info(`Creating role policy: ${policyName}`);
+
+    await iam.putRolePolicy({
+        PolicyDocument: JSON.stringify(policyDoc),
+        PolicyName: policyName,
+        RoleName: fullRoleName
+    }).promise();
+
+    try {
+        logger.info(`Created role policy`);
+        await continuation(arn);
+    } finally {
+        logger.info(`Deleting role policy`);
+        await iam.deleteRolePolicy({
+            PolicyName: policyName,
+            RoleName: fullRoleName
+        }).promise();
+        logger.info(`Role policy deleted`);
+    }
+};
+
+const withLambda =
+    (functionName, roleArn, src = `../${functionName}.js`) =>
+        async continuation => {
+            logger.info(`Zipping lambda...`);
+            const zip = new JSZip();
+            zip.file(
+                `${functionName}.js`,
+                await readFile(src)
+            );
+            logger.info(`Zipped`);
+            logger.info(`Creating lambda: ${functionName}`);
+            const buffer = await zip.generateAsync({ type: "nodebuffer" });
+            const resp = await lambda.createFunction({
+                FunctionName: functionName,
+                Handler: "lambda-with-ssm-param-resolver.handler",
+                Runtime: "nodejs10.x",
+                Code: {
+                    ZipFile: buffer
+                },
+                Role: roleArn
+            });
+            logger.info(`Lambda created ${util.inspect(resp)}`);
+            try {
+                await continuation();
+            } finally {
+                logger.info(`Deleting lambda`);
+                await lambda.deleteFunction({
+                    FunctionName: functionName
+                });
+                logger.info(`Lambda deleted`);
+            }
+        };
+
+module.exports = { withRole, withLambda };
---
+++ integration-tests/src/middlewares/ssm-param-resolver/__test__/lambda-with-ssm-param-resolver.test.js
@@ -0,0 +1,91 @@
+const test = require('ninos')(require('ava'));
+const AWS = require('aws-sdk');
+const logger = console;
+const { withRole, withLambda } = require('../../../../integration-test-utils');
+const util = require('util');
+
+const lambda = new AWS.Lambda({ region: process.env.AWS_DEFAULT_REGION });
+const ssm = new AWS.SSM({ region: process.env.AWS_DEFAULT_REGION });
+
+const functionName = "lambda-with-ssm-param-resolver";
+const roleName = "SSM-Int-Test";
+
+const policy_doc = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "logs:CreateLogGroup",
+            "Resource": `arn:aws:logs:${process.env.AWS_DEFAULT_REGION}:${process.env.AWS_ACCOUNT_ID}:*`
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+                `arn:aws:logs:eu-west-2:447213725459:log-group:/aws/lambda/${functionName}:*`
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": "ssm:GetParameters",
+            "Resource": "*"
+        },
+    ]
+}
+
+const withSsmParams = params => async continuation => {
+    try {
+        for (const param of params) {
+            await ssm.putParameter(param).promise();
+            logger.info(`Created param with name: ${param.Name}`);
+        }
+
+        await continuation();
+    } finally {
+        logger.info(`Deleting parameters`);
+        await ssm.deleteParameters({ Names: params.map(p => p.Name) }).promise();
+        logger.info(`Parameters deleted`);
+    }
+};
+
+test('SSM Parameter Resolving Middleware - Integration test', async t => {
+    await withRole(
+        roleName,
+        policy_doc
+    )(
+        async roleArn => withLambda(
+            functionName,
+            roleArn,
+            "integration-tests/dist/index.js"
+        )(
+            async () => withSsmParams([
+                {
+                    Name: "/mambda/test/A",
+                    Type: "String",
+                    Value: "hula"
+                },
+                {
+                    Name: "/mambda/test/B",
+                    Type: "StringList",
+                    Value: "[\"hula\"]"
+                }
+            ])(
+                async () => {
+
+                    logger.info(`Invoking lambda`);
+                    const result = await lambda.invoke({
+                        FunctionName: functionName,
+                        InvocationType: "RequestResponse",
+                        Payload: JSON.stringify({})
+                    }).promise();
+                    logger.info(`Result of invoking lambda: ${util.inspect(result)}`);
+
+                    // TODO assert we read our params back
+                }
+            )
+        )
+    );
+});
