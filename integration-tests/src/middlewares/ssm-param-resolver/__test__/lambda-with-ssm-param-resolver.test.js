5a98f9a2d1b5f4488c83886bb640718648febcc3
Matt Searle <mattsearle@ymail.com>
2019-07-22 13:18:25 +0100

    style(integration tests): Fix linting errors

--- integration-tests/src/middlewares/ssm-param-resolver/__test__/lambda-with-ssm-param-resolver.test.js
+++ integration-tests/src/middlewares/ssm-param-resolver/__test__/lambda-with-ssm-param-resolver.test.js
@@ -1,91 +1,86 @@
+const util = require('util');
 const test = require('ninos')(require('ava'));
 const AWS = require('aws-sdk');
-const logger = console;
-const { withRole, withLambda } = require('../../../../integration-test-utils');
-const util = require('util');
 
+const { withRole, withLambda } = require('../../../../integration-test-utils');
+
+const logger = console;
 const lambda = new AWS.Lambda({ region: process.env.AWS_DEFAULT_REGION });
 const ssm = new AWS.SSM({ region: process.env.AWS_DEFAULT_REGION });
 
-const functionName = "lambda-with-ssm-param-resolver";
-const roleName = "SSM-Int-Test";
+const functionName = 'lambda-with-ssm-param-resolver';
+const roleName = 'SSM-Int-Test';
 
-const policy_doc = {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "logs:CreateLogGroup",
-            "Resource": `arn:aws:logs:${process.env.AWS_DEFAULT_REGION}:${process.env.AWS_ACCOUNT_ID}:*`
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": [
-                `arn:aws:logs:eu-west-2:447213725459:log-group:/aws/lambda/${functionName}:*`
-            ]
-        },
-        {
-            "Effect": "Allow",
-            "Action": "ssm:GetParameters",
-            "Resource": "*"
-        },
-    ]
-}
-
-const withSsmParams = params => async continuation => {
-    try {
-        for (const param of params) {
-            await ssm.putParameter(param).promise();
-            logger.info(`Created param with name: ${param.Name}`);
-        }
-
-        await continuation();
-    } finally {
-        logger.info(`Deleting parameters`);
-        await ssm.deleteParameters({ Names: params.map(p => p.Name) }).promise();
-        logger.info(`Parameters deleted`);
-    }
+const policyDocument = {
+	Version: '2012-10-17',
+	Statement: [
+		{
+			Effect: 'Allow',
+			Action: 'logs:CreateLogGroup',
+			Resource: `arn:aws:logs:${process.env.AWS_DEFAULT_REGION}:${process.env.AWS_ACCOUNT_ID}:*`
+		},
+		{
+			Effect: 'Allow',
+			Action: ['logs:CreateLogStream', 'logs:PutLogEvents'],
+			Resource: [
+				`arn:aws:logs:eu-west-2:447213725459:log-group:/aws/lambda/${functionName}:*`
+			]
+		},
+		{
+			Effect: 'Allow',
+			Action: 'ssm:GetParameters',
+			Resource: '*'
+		}
+	]
 };
 
-test('SSM Parameter Resolving Middleware - Integration test', async t => {
-    await withRole(
-        roleName,
-        policy_doc
-    )(
-        async roleArn => withLambda(
-            functionName,
-            roleArn,
-            "integration-tests/dist/index.js"
-        )(
-            async () => withSsmParams([
-                {
-                    Name: "/mambda/test/A",
-                    Type: "String",
-                    Value: "hula"
-                },
-                {
-                    Name: "/mambda/test/B",
-                    Type: "StringList",
-                    Value: "[\"hula\"]"
-                }
-            ])(
-                async () => {
+const withSsmParams = params => async continuation => {
+	try {
+		await Promise.all(
+			params.map(param =>
+				ssm
+					.putParameter(param)
+					.promise()
+					.then(logger.info(`Created param with name: ${param.Name}`))
+			)
+		);
 
-                    logger.info(`Invoking lambda`);
-                    const result = await lambda.invoke({
-                        FunctionName: functionName,
-                        InvocationType: "RequestResponse",
-                        Payload: JSON.stringify({})
-                    }).promise();
-                    logger.info(`Result of invoking lambda: ${util.inspect(result)}`);
+		await continuation();
+	} finally {
+		logger.info(`Deleting parameters`);
+		await ssm.deleteParameters({ Names: params.map(p => p.Name) }).promise();
+		logger.info(`Parameters deleted`);
+	}
+};
 
-                    // TODO assert we read our params back
-                }
-            )
-        )
-    );
+test('SSM Parameter Resolving Middleware - Integration test', async () => {
+	await withRole(roleName, policyDocument)(async roleArn =>
+		withLambda(functionName, roleArn, 'integration-tests/dist/index.js')(
+			async () =>
+				withSsmParams([
+					{
+						Name: '/mambda/test/A',
+						Type: 'String',
+						Value: 'hula'
+					},
+					{
+						Name: '/mambda/test/B',
+						Type: 'StringList',
+						Value: '["hula"]'
+					}
+				])(async () => {
+					logger.info(`Invoking lambda`);
+					const result = await lambda
+						.invoke({
+							FunctionName: functionName,
+							InvocationType: 'RequestResponse',
+							Payload: JSON.stringify({})
+						})
+						.promise();
+					logger.info(`Result of invoking lambda: ${util.inspect(result)}`);
+
+					// TODO assert we read our params back
+				})
+		)
+	);
 });
--- integration-tests/src/middlewares/ssm-param-resolver/lambda-with-ssm-param-resolver.js
+++ integration-tests/src/middlewares/ssm-param-resolver/lambda-with-ssm-param-resolver.js
@@ -1,20 +1,20 @@
 // TODO It seems that ava's sources are not added to the node resolution path.
+/* eslint-disable import/no-unresolved */
 const lambda = require('../../../../dist');
 const ssmParamResolver = require('../../../../dist/middlewares/ssm-param-resolver');
+/* eslint-enable */
 
-const parametersToLoad = [
-    "/mambda/test/A",
-    "/mambda/test/B"
-]
+const parametersToLoad = ['/mambda/test/A', '/mambda/test/B'];
 
 async function ssmParamConsumer(event) {
-    return {
-        A: event._ssmParams["/mambda/test/A"],
-        B: event._ssmParams["/mambda/test/B"],
-    };
+	return {
+		A: event._ssmParams['/mambda/test/A'],
+		B: event._ssmParams['/mambda/test/B']
+	};
 }
 
-exports.handler = lambda(ssmParamConsumer)
-    .use(ssmParamResolver({
-        parameterNames: parametersToLoad
-    }));
+exports.handler = lambda(ssmParamConsumer).use(
+	ssmParamResolver({
+		parameterNames: parametersToLoad
+	})
+);
