04e99b54b39969969f3d4c7b29bbc499a41c3b8f
Matthew Amos <mamos@scottlogic.com>
2019-08-01 12:55:57 +0100

    Fix lint issuess

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-	plugins: ["remark-preset-lint-styleguide", "remark-preset-lint-recommended", ["remark-lint-no-html", false], ["remark-lint-no-tabs", false]]
+	plugins: [
+		"remark-preset-lint-style-guide",
+		"remark-preset-lint-recommended",
+		[
+			"remark-lint-no-html",
+			false
+		],
+		[
+			"remark-lint-no-tabs",
+			false
+		]
+	]
 }
